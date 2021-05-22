610d4f256e0b4dcd43517aad572bb2ff0e595c8a
tnomura2467 <twio2467@gmail.com>
2021-05-22 20:41:35 +0900

    add Xline

---
+++ ros-sharp-support-for-uwp/Unity3D/Assets/Origin/Scripts/LineScripts/LineControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineControl : MonoBehaviour
+{
+    public GameObject newLine;
+    LineRenderer lRend;
+
+    public GameObject bottle;
+    public GameObject Head1;
+
+    [SerializeField] private Gradient _gradient;
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        newLine = new GameObject("Line1");
+        lRend = newLine.AddComponent<LineRenderer>();
+
+        lRend.numCapVertices = 10;
+        lRend.numCornerVertices = 10;
+        lRend.startWidth = 0.02f;
+        lRend.endWidth = 0.02f;
+
+        lRend.material = new Material(Shader.Find("Sprites/Default"));
+        lRend.colorGradient = _gradient;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lRend.SetPosition(0, Head1.gameObject.transform.position);
+        lRend.SetPosition(1, bottle.gameObject.transform.position);
+    }
+
+}
