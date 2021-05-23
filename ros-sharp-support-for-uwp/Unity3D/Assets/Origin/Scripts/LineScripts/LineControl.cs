8001096cd20962687e236a87e6020576b5a5bd00
tnomura2467 <twio2467@gmail.com>
2021-05-23 21:19:06 +0900

    issue Xcross

--- ros-sharp-support-for-uwp/Unity3D/Assets/Origin/Scripts/LineScripts/LineControl.cs
+++ ros-sharp-support-for-uwp/Unity3D/Assets/Origin/Scripts/LineScripts/LineControl.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class LineControl : MonoBehaviour
 {
     public GameObject newLine;
+    public GameObject newLine2;
     LineRenderer lRend;
+    LineRenderer lRend2;
 
-    public GameObject bottle;
-    public GameObject Head1;
+    public GameObject Object1;
+    public GameObject Human1;
+    public GameObject Human2;
+    public GameObject Icon;
+    public GameObject Icon2;
 
     [SerializeField] private Gradient _gradient;
+    [SerializeField] private Gradient _gradient2;
     private void Awake()
     {
         
@@ -19,22 +25,70 @@ public class LineControl : MonoBehaviour
     void Start()
     {
         newLine = new GameObject("Line1");
+        newLine2 = new GameObject("Line2");
         lRend = newLine.AddComponent<LineRenderer>();
+        lRend2 = newLine2.AddComponent<LineRenderer>();
 
         lRend.numCapVertices = 10;
         lRend.numCornerVertices = 10;
-        lRend.startWidth = 0.02f;
-        lRend.endWidth = 0.02f;
-
+        lRend.startWidth = 0.01f;
+        lRend.endWidth = 0.01f;
         lRend.material = new Material(Shader.Find("Sprites/Default"));
-        lRend.colorGradient = _gradient;
+        lRend.colorGradient = _gradient2;
+
+        lRend2.numCapVertices = 10;
+        lRend2.numCornerVertices = 10;
+        lRend2.startWidth = 0.01f;
+        lRend2.endWidth = 0.01f;
+        lRend2.material = new Material(Shader.Find("Sprites/Default"));
+        lRend2.colorGradient = _gradient;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        lRend.SetPosition(0, Head1.gameObject.transform.position);
-        lRend.SetPosition(1, bottle.gameObject.transform.position);
+        lRend.SetPosition(0, Icon.gameObject.transform.position);
+
+        if (VisibleHuman.isObject1Render == true && VisibleHuman.isHuman1Render == true)
+        {
+            lRend.SetPosition(0, Human1.gameObject.transform.position);
+            lRend.SetPosition(1, Object1.gameObject.transform.position);
+            
+        }else if (VisibleHuman.isObject1Render == true)
+        {
+            lRend.SetPosition(1, Object1.gameObject.transform.position);
+
+        }else
+        {
+            lRend.SetPosition(1, Human1.gameObject.transform.position);
+        }
+
+
+
+        lRend2.SetPosition(0, Icon2.gameObject.transform.position);
+        if (VisibleHuman.isObject1Render == true && VisibleHuman.isHuman2Render == true)
+        {
+            lRend2.SetPosition(0, Human2.gameObject.transform.position);
+            lRend2.SetPosition(1, Object1.gameObject.transform.position);
+        }
+        else if (VisibleHuman.isObject1Render == true)
+        {
+            lRend2.SetPosition(1, Object1.gameObject.transform.position);
+        }
+        else
+        {
+            lRend2.SetPosition(1, Human2.gameObject.transform.position);
+        }
+
+
+        VisibleHuman.isObject1Render = false;
+        VisibleHuman.isHuman1Render = false;
+        VisibleHuman.isHuman2Render = false;
     }
+
+
 
 }
---
+++ ros-sharp-support-for-uwp/Unity3D/Assets/Origin/Scripts/LineScripts/VisibleHuman.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Microsoft.MixedReality.Toolkit;
+
+public class VisibleHuman : MonoBehaviour
+{
+    //メインカメラに付いているタグ名
+    private const string MAIN_CAMERA_TAG_NAME = "MainCamera";
+
+    //カメラに表示されているか
+    private bool _isRendered = false;
+
+    public static bool isObject1Render = false;
+    public static bool isHuman1Render = false;
+    public static bool isHuman2Render = false;
+
+
+    private void Update()
+    {
+
+       
+    }
+
+    //カメラに映ってる間に呼ばれる
+    private void OnWillRenderObject()
+    {
+        //メインカメラに映った時だけ_isRenderedを有効に
+        if (Camera.current.tag == MAIN_CAMERA_TAG_NAME)
+        {
+            if (this.gameObject.tag == "Object1")
+            {
+                isObject1Render = true;
+            }
+            if (this.gameObject.tag == "human1")
+            {
+                isHuman1Render = true;
+            }
+            if (this.gameObject.tag == "human2")
+            {
+                isHuman2Render = true;
+            }
+
+        }
+    }
+
+}
