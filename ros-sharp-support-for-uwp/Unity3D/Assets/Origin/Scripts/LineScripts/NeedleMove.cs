81405a52e27a702c1a23a672862e0b66fa0e8066
tnomura2467 <twio2467@gmail.com>
2021-07-27 15:15:44 +0900

    add visual objects

---
+++ ros-sharp-support-for-uwp/Unity3D/Assets/Origin/Scripts/LineScripts/NeedleMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 FirstPos;
+    private Vector3 transRota;
+
+    void Start()
+    {
+        FirstPos = this.gameObject.transform.localPosition;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transRota.z = this.gameObject.transform.rotation.z;
+        transRota.x = 0.0f;
+        transRota.y = 0.0f;
+        this.gameObject.transform.localPosition = FirstPos;
+        this.transform.rotation= Quaternion.Euler(transRota);
+
+    }
+}
